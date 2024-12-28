be45fc38a145f96159c2dd6e9672612b59aa6f95
thstkddnr20 <thstkddnr20@naver.com>
2024-12-28 18:47:01 +0900

    직접 구현한 ArrayList

---
+++ src/studyJava/dataStructure/collection/arraylist/ArrayListMain.java
@@ -0,0 +1,16 @@
+package studyJava.dataStructure.collection.arraylist;
+
+
+public class ArrayListMain {
+    public static void main(String[] args) {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        list.add(1);
+        list.add(2);
+
+        list.remove(0);
+        list.remove(Integer.valueOf(2));
+
+        System.out.println(list);
+        System.out.println(list.size());
+    }
+}
