832e45f176f74b812cc9905511d942211821bef5
thstkddnr20 <thstkddnr20@naver.com>
2025-01-01 14:40:05 +0900

    MyStack 구현 필요

---
+++ src/studyJava/dataStructure/collection/stack/MyStack.java
@@ -0,0 +1,15 @@
+package studyJava.dataStructure.collection.stack;
+
+import java.util.Vector;
+
+public class MyStack<E> extends Vector<E> {
+
+    //제일 마지막 요소를 꺼내오는 peek()
+
+    //값을 집어넣는 push()
+
+    //제일 마지막 요소를 꺼내는 pop()
+
+    //비어있는 경우라면 true를 반환하는 empty()
+
+}
